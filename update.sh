13622e87d64ae130244df1f0280e109731780eb3
kostya1661@gmail.com <!#%&(@$^*)lokin135kostya!#%&(@$^*)>
2022-02-26 15:04:52 +0300

    Added update script

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Pull git changes.."
+git pull
+
+echo "MoneySaver stopping..."
+docker-compose stop
+echo "Pull MS changes..."
+docker-compose pull
+echo "Staring MoneySaver..."
+docker-compose up -d
+
